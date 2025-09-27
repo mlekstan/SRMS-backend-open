9c3ff9cfffc7096f12e4f4c498b304e68b942616
Michal Lekstan <m.lekstan@interia.pl>
2025-09-27 23:24:52 +0200

    Closes #1; https

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync("e:\\Studia_Teleinformatyka_2022_2023\\Praca_inżynierska\\Cert\\localhost-key.pem"),
+  cert: fs.readFileSync("e:\\Studia_Teleinformatyka_2022_2023\\Praca_inżynierska\\Cert\\localhost.pem")
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions });
   app.enableCors()
   await app.listen(process.env.PORT ?? 3000);
 }
